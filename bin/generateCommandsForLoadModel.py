89a1814997845fb8fcee361d25f615137cedbde9
cansyl <cansyl.lab@gmail.com>
2018-08-15 16:50:38 +0300

    Scripts were added to load saved models and predict whole ChEMBL

--- bin/generateCommandsForLoadModel.py
+++ bin/generateCommandsForLoadModel.py
@@ -14,7 +14,7 @@ count = 0
 for line in lst_best_fl[1:]:
     count+=1
     log_fl, modelname, target, optimizer, learning_rate, epoch, hidden1, hidden2, dropout, rotate, save_model, test_f1score, test_mcc, test_accuracy, test_precision, test_recall, test_tp, test_fp, test_tn, test_fn, test_threshold, val_auc, val_auprc, test_auc, test_auprc = line.split("\t")
-    print("bsub -q research -R 'select[nprocs<=2]' -M 10240 -R 'rusage[mem=30720]'  -o ../LOGS/LoadModels/loadModel_{}.out \"python loadModel.py {}\"".format(target, target))
+    print("bsub -q research -R 'select[nprocs<=2]' -M 10240 -R 'rusage[mem=10240]'  -o ../LOGS/LoadModels/loadModel_{}.out \"python loadModel.py {}\"".format(target, target))
     print("sleep 10")
 
 
