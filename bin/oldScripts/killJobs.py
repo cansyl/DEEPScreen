b2436192cc960c44c426ff4fe285ad50dd918dfa
cansyl <cansyl.lab@gmail.com>
2019-01-07 16:18:55 +0000

    unnecessary files removed

--- bin/oldScripts/killJobs.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-fl = "/hps/nobackup/production/uniprot/rahmet/DEEPScreen/LOGS/all_jobs.txt"
-with open(fl) as fl:
-	for line in fl:
-		subprocess.call(["bkill", line.split(" ")[0]])
-		#print(line.split(" "))
